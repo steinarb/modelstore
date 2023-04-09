7bac6d42b829ea3ca6d68193fef0e846dcb94255
steinarb <sb@dod.no>
2023-04-09 19:14:04 +0000

    Deploying to gh-pages from @ steinarb/modelstore@17394cca96631b6fc4ce17e3177346567e35db49 🚀

--- javadoc/package-search-index.js
+++ javadoc/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"no.priv.bang.modeling.modelstore.backend"},{"l":"no.priv.bang.modeling.modelstore.services"}]
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"no.priv.bang.modeling.modelstore.backend"},{"l":"no.priv.bang.modeling.modelstore.services"},{"l":"no.priv.bang.modeling.modelstore.value"}]
